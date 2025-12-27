a0fbcfea6e03d45a5fb13f9798b141c592b5eae5
Harold Martin <harold.martin@gmail.com>
2025-12-27 13:53:52 -0600

    no message

--- lib/db/migrations/0010_sharp_webhook_unique.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM "WebhookLog" a
-USING "WebhookLog" b
-WHERE a."messageSid" = b."messageSid"
-AND a."messageSid" IS NOT NULL
-AND a.ctid < b.ctid;
---> statement-breakpoint
-CREATE UNIQUE INDEX "WebhookLog_messageSid_unique" ON "WebhookLog" ("messageSid");
